f3a5137b139790ce2fe1aa6e68de2cd39972efc9
martin <martin.dekov@sofyma.com>
2018-04-27 16:19:40 +0300

    minor fixes

--- resources/views/page/post/post.blade.php
+++ resources/views/page/post/post.blade.php
@@ -12,22 +12,23 @@
                     <small>By : {{$post->comment->user->name}}</small>
                 @endforeach
             @endif
+            <hr>
             <p><a class="btn btn-primary btn-lg" href="/" role="button">Back &raquo;</a></p>
             @if(Auth::user())
                 @if(Auth::user()->id==$post->user->id)
                     <a href="/edit/{{$post->id}}"><p>Edin post</p></a>
                     <a href="/delete/{{$post->id}}"><p>Delete post</p></a>
                 @endif
-            @endif
             <form action="/comment" method="POST" class="form-group">
             @csrf
             <textarea class="form-control" name="comment_body" id="" cols="45" rows="5"></textarea>
             <input type="hidden" name="user_id" id="" value="{{Auth::user()->id}}">
             <input type="hidden" name="post_id" id="" value="{{$post->id}}">
-        <div>
-        <button class="btn btn-primary btn-lg" type="submit">Comment</button>
-      </div>
-      </form>
+                <div>
+                <button class="btn btn-primary btn-lg" type="submit">Comment</button>
+                </div>
+            </form>
+            @endif
         </div>
     </div>
 @endsection
